7958bb3be72588c0300d96765d9ebde8f3858b0e
jiyongchun <wswu.shan@163.com>
2023-09-28 17:37:32 +0800

    second commit

--- mycompose/src/main/java/com/jerry/mycompose/MainActivity.kt
+++ mycompose/src/main/java/com/jerry/mycompose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.jerry.mycompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    Log.i(">>>", "asdf")
                 }
             }
         }
